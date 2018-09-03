e6f92ef3fade5469cdb343fa656216de2583d11c
salarhp <salarhoseinpuor@gmail.com>
2018-09-03 18:03:06 +0430

    crud category and remove invalid file

--- rBlog/app/controllers/admin/categories_controller.rb
+++ rBlog/app/controllers/admin/categories_controller.rb
@@ -1,17 +1,38 @@
 class Admin::CategoriesController < Admin::ApplicationController
   def new
+    @page_title = 'Add cateory'
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice]="category created"
+      redirect_to admin_categories_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice]="category updated"
+      redirect_to admin_categories_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:notice]="category deleted"
+    redirect_to admin_categories_path
   end
 
   def index
@@ -19,5 +40,10 @@ class Admin::CategoriesController < Admin::ApplicationController
   end
 
   def show
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
